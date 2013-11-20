3708e8587a1d00e12fe2da364396c331f503a82d
manleyhimself <bcmanley1@gmail.com>
2013-11-20 17:29:56 -0500

    security intact

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user! 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+
+  def after_sign_in_path_for(resource)
+    tournaments_path
+  end
+
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
