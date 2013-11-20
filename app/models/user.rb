39960bc0a9178080dc1d1fd4352f50fc385c16d1
samueldowens <samueldowens@gmail.com>
2013-11-20 13:03:47 -0500

    sign up email works

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :user_tournaments
   has_many :tournaments, through: :user_tournaments
+
+  after_create :send_welcome_email 
+
+
+  private
+
+  def send_welcome_email
+    UserMailer.welcome_email(self).deliver
+  end
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,4 +28,14 @@ SwissMtg::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => 'smtp.gmail.com',
+      :port                 => 587,
+      :domain               => 'gmail.com',
+      :user_name            => 'team.monocle.tournio@gmail.com',
+      :password             => 'pinkyout',
+      :authentication       => 'login',
+      :enable_starttls_auto => true
+  }
 end
