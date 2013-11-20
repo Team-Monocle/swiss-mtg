db66cca5fb7d29f8a033ac86b07874ef23d3afa1
J-Scag <j_scag@yahoo.com>
2013-11-20 15:02:58 -0500

    STYLES FOR MILES

--- app/assets/javascripts/tournaments.js
+++ app/assets/javascripts/tournaments.js
@@ -1,6 +1,9 @@
+
 $(function(){
-//get height for 2
-var col2Height = $('#pairing_sidebar').height();
-//set height for 1
-$('#player_sidebar').height(col2Height);
+  //get height for 2
+  var barHeight = $('#pairing_sidebar').height();
+  var bar2Height = $('.tab-pane').height();
+  var col2Height = barHeight + bar2Height;
+  //set height for 1
+  $('#player_sidebar').height(col2Height);
 });
