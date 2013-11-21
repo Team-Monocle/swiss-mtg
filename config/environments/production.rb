da2d5d84b636c36259c179f9cb3b7432f23853e2
samueldowens <samueldowens@gmail.com>
2013-11-21 11:20:55 -0500

    continuing bug fix attempts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,8 @@ SwissMtg::Application.configure do
 
   # raise "FORGOT TO SET default_url_options IN CONFIG/ENVIRONMENTS/PRODUCTION"
   config.action_mailer.default_url_options = { :host => 'tourn-io.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address              => 'smtp.gmail.com',
